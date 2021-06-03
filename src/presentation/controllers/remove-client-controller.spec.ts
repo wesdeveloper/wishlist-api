532ec098dae26d22d006e7ed1bb1bf6d602eb97f
wesdeveloper <wesleyalopes2010@gmail.com>
2021-06-03 20:19:02 -0300

    test: Remove client controller

---
+++ src/presentation/controllers/remove-client-controller.spec.ts
@@ -0,0 +1,71 @@
+import Chance from 'chance';
+import { RemoveClient } from '../../domain/usecases';
+import { HttpRequest, HttpResponse } from '../protocols';
+import { RemoveClientController } from './remove-client-controller';
+
+const chance = new Chance();
+
+const makeRemoveClientUseCase = () => {
+  class RemoveClientSpy implements RemoveClient {
+    // eslint-disable-next-line
+    remove = async (clientId: number): Promise<boolean> => true;
+  }
+
+  return new RemoveClientSpy();
+};
+
+const makeSut = () => {
+  const removeClientSpy = makeRemoveClientUseCase();
+  const sut = new RemoveClientController(removeClientSpy);
+
+  return {
+    sut,
+    removeClientSpy,
+  };
+};
+
+describe('Remoce Client Controller', () => {
+  describe('Success cases', () => {
+    it('Should remove a client', async () => {
+      const { sut } = makeSut();
+      const clientId = chance.integer();
+
+      const httpRequest: HttpRequest = {
+        params: { clientId },
+      };
+
+      const httpResponse: HttpResponse = await sut.handle(httpRequest);
+      expect(httpResponse.status).toBe(204);
+    });
+  });
+
+  describe('Error cases', () => {
+    describe('Not found error', () => {
+      it('Should remove a client that not exists', async () => {
+        const { sut, removeClientSpy } = makeSut();
+        jest.spyOn(removeClientSpy, 'remove').mockImplementationOnce(() => Promise.resolve(false));
+
+        const httpRequest: HttpRequest = {
+          params: { clientId: chance.integer() },
+        };
+        const httpResponse: HttpResponse = await sut.handle(httpRequest);
+        expect(httpResponse.status).toBe(404);
+      });
+    });
+
+    describe('Internal server error', () => {
+      it('Should remove a client and receive internal server error', async () => {
+        const { sut, removeClientSpy } = makeSut();
+        jest.spyOn(removeClientSpy, 'remove').mockImplementationOnce(async () => {
+          throw new Error();
+        });
+
+        const httpRequest: HttpRequest = {
+          params: { clientId: chance.integer() },
+        };
+        const httpResponse: HttpResponse = await sut.handle(httpRequest);
+        expect(httpResponse.status).toBe(500);
+      });
+    });
+  });
+});
