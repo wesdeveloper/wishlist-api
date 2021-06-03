660936517d38eb8492f7bf273a0e8df43ec8446a
wesdeveloper <wesleyalopes2010@gmail.com>
2021-06-03 19:56:48 -0300

    chore: change docker settings

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -9,10 +9,6 @@ beforeAll(async () => {
   global.testRequest = supertest(server);
 });
 
-afterEach(async () => {
-  await database.getConnection()('client').delete();
-});
-
 afterAll(async () => {
   await database.closeConnection();
 });
