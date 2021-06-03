e4472ed32f7a02dce2e7f0d83c6512a03401a42b
wesdeveloper <wesleyalopes2010@gmail.com>
2021-06-03 13:15:48 -0300

    feat: Fetch client factory

--- src/main/factories/client.ts
+++ src/main/factories/client.ts
@@ -1,13 +1,23 @@
+import { FetchClientController } from '../../presentation/controllers/fetch-client-controller';
 import database from '../../infra/db/mysql/db';
-import { CreateClientUseCase } from '../../data/usecases/client/create-client';
+import { CreateClientUseCase, FetchClientUseCase } from '../../data/usecases';
 import { ClientRepository } from '../../infra/db/mysql/client-repository/client-repository';
 import { CreateClientController } from '../../presentation/controllers';
 
+const dbConnection = database.getConnection();
+
 export const makeCreateClientController = (): CreateClientController => {
-  const dbConnection = database.getConnection();
   const createClientRepository = new ClientRepository(dbConnection);
   const createClient = new CreateClientUseCase(createClientRepository);
   const createClientController = new CreateClientController(createClient);
 
   return createClientController;
+};
+
+export const makeFetchClientController = (): FetchClientController => {
+  const fetchClientRepository = new ClientRepository(dbConnection);
+  const fetchClient = new FetchClientUseCase(fetchClientRepository);
+  const fetchClientController = new FetchClientController(fetchClient);
+
+  return fetchClientController;
 };
