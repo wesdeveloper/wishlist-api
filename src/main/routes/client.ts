356b85b60e98790b4bd76031fe66472b2892d4bb
wesdeveloper <wesleyalopes2010@gmail.com>
2021-06-03 13:36:45 -0300

    feat: Fetch client route

--- src/main/routes/client.ts
+++ src/main/routes/client.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { expressRouterAdapter } from '../adapters/express-route-adapter';
-import { makeCreateClientController } from '../factories/client';
+import { makeCreateClientController, makeFetchClientController } from '../factories/client';
 import BaseRoutes from './protocols/base-routes';
 
 class ClientRoutes extends BaseRoutes {
   loadRoutes(router: Router) {
     const createClientController = makeCreateClientController();
-    router.post('/client', expressRouterAdapter(createClientController));
+    const fetchClientController = makeFetchClientController();
+
+    router
+      .post('/client', expressRouterAdapter(createClientController))
+      .get('/client', expressRouterAdapter(fetchClientController));
   }
 }
 
