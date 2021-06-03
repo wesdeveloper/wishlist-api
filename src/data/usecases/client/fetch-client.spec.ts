948fa474608c3becfdde0eaf4d2a63624eb32a35
wesdeveloper <wesleyalopes2010@gmail.com>
2021-06-03 12:37:26 -0300

    test: Fetch client data usecase

---
+++ src/data/usecases/client/fetch-client.spec.ts
@@ -0,0 +1,36 @@
+import { FetchClientModel } from 'src/domain/models/client';
+import { FetchClientRepository } from '../../protocols/client/fetch-client-repository';
+import { FetchClientUseCase } from './fetch-client';
+
+const makeFetchClientRepository = () => {
+  class FetchClientUseRepoStub implements FetchClientRepository {
+    fetch = async (page = 1, pageSize = 10): Promise<FetchClientModel> => {
+      const fetchedClientData = {
+        clients: [],
+        pagination: {
+          page,
+          pageSize,
+        },
+      };
+
+      return fetchedClientData;
+    };
+  }
+  return new FetchClientUseRepoStub();
+};
+
+const makeSut = () => {
+  const fetchClientRepoStub = makeFetchClientRepository();
+  const sut = new FetchClientUseCase(fetchClientRepoStub);
+
+  return { sut };
+};
+
+describe('Fetch Clients usecase', () => {
+  it('Should fetch clients', async () => {
+    const { sut } = makeSut();
+    const fetchedClientData = await sut.fetch();
+
+    expect(fetchedClientData).toEqual(expect.objectContaining({ clients: expect.any(Array), pagination: expect.any(Object) }));
+  });
+});
