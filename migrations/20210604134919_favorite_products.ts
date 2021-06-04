ad557a2ab68bc2fd1ca6879dbd135728264f349e
wesdeveloper <wesleyalopes2010@gmail.com>
2021-06-04 14:59:00 -0300

    chore: add favorite_products migration

---
+++ migrations/20210604134919_favorite_products.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTableIfNotExists('favorite_products', (table: Knex.TableBuilder) => {
+    table.integer('client_id').unsigned().nullable();
+    table.string('product_id').notNullable();
+    table.timestamps(true, true);
+
+    table.foreign('client_id')
+      .references('client.id')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('favorit_produtcs');
+}
